74420ea0b82db770baabc0d1367fe8307f21323b
pedro_marcello <pedro.marcello.q@gmail.com>
2020-02-10 18:48:10 -0300

    Primeiro Commit

---
+++ Mvc_Angular_Crud_VBNET/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Mvc_Angular_Crud_VBNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Angular_Crud_VBNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetProdutos()
+        {
+            using (CadastroEntities ctx = new CadastroEntities())
+            {
+                try
+                {
+                    var produtos = ctx.Produtos.ToList();
+                    return Json(produtos, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
---
+++ Mvc_Angular_Crud_VBNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc_Angular_Crud_VBNET.MvcApplication" Language="C#" %>
